319c97c769cfe0796f9bc7fa9abcc39604a6be7a
Artur Ratajczak <artur.ratajczak1@gmail.com>
2019-03-30 13:53:22 +0100

    sorting, filtering and rating

--- RNDM/Controller/MainVC.swift
+++ RNDM/Controller/MainVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class MainVC: UIViewController {
 
@@ -14,6 +15,9 @@ class MainVC: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
     
     private var thoughts = [Thought]()
+    private var thoughtsCollectionRef: CollectionReference!
+    private var thoughtsListener: ListenerRegistration!
+    private var selectedCategory = ThoughtCategory.funny.rawValue
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +25,62 @@ class MainVC: UIViewController {
         tableView.dataSource = self
         tableView.estimatedRowHeight = 80
         tableView.rowHeight = UITableView.automaticDimension
+        
+        thoughtsCollectionRef = Firestore.firestore().collection(THOUGHTS_REF)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setListener()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        thoughtsListener.remove()
+    }
+    
+    func setListener() {
+        if selectedCategory == ThoughtCategory.popular.rawValue {
+            thoughtsListener = thoughtsCollectionRef
+                .order(by: NUM_LIKES, descending: true )
+                .addSnapshotListener { (snapshot, error) in
+                    if let error = error {
+                        debugPrint("Error featching docs \(error)")
+                    } else {
+                        self.thoughts.removeAll()
+                        self.thoughts = Thought.parseData(snapshot: snapshot)
+                        self.tableView.reloadData()
+                    }
+            }
+        } else {
+            thoughtsListener = thoughtsCollectionRef
+                .whereField(CATEGORY, isEqualTo: selectedCategory)
+                .order(by: TIMESTAMP, descending: true )
+                .addSnapshotListener { (snapshot, error) in
+                if let error = error {
+                    debugPrint("Error featching docs \(error)")
+                } else {
+                    self.thoughts.removeAll()
+                    self.thoughts = Thought.parseData(snapshot: snapshot)
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
 
-
+    @IBAction func categoryChanged(_ sender: Any) {
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            selectedCategory = ThoughtCategory.funny.rawValue
+        case 1:
+            selectedCategory = ThoughtCategory.serious.rawValue
+        case 2:
+            selectedCategory = ThoughtCategory.crazy.rawValue
+        default:
+            selectedCategory = ThoughtCategory.popular.rawValue
+        }
+        
+        thoughtsListener.remove()
+        setListener()
+    }
 }
 
 extension MainVC: UITableViewDelegate, UITableViewDataSource {
--- RNDM/View/ThoughtCell.swift
+++ RNDM/View/ThoughtCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ThoughtCell: UITableViewCell {
 
@@ -16,13 +17,29 @@ class ThoughtCell: UITableViewCell {
     @IBOutlet weak var likesNumLbl: UILabel!
     @IBOutlet weak var likesImg: UIImageView!
     
+    private var thought: Thought!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        let tap = UITapGestureRecognizer(target: self, action: #selector(likeTapped))
+        likesImg.isUserInteractionEnabled = true
+        likesImg.addGestureRecognizer(tap)
+    }
+    
+    @objc func likeTapped(){
+        Firestore.firestore().collection(THOUGHTS_REF).document(thought.documentId).setData([NUM_LIKES: thought.numLikes + 1], merge: true)
     }
 
-    func configureCell(for: Thought) {
+    func configureCell(for thought: Thought) {
+        self.thought = thought
+        usernameLbl.text = thought.username
+        thoughtTxtLbl.text = thought.thoughtTxt
+        likesNumLbl.text = String(thought.numLikes)
         
+        let formater = DateFormatter()
+        formater.dateFormat = "d MMM, hh:mm"
+        let timestampe = formater.string(from: thought.timestamp)
+        timestampLbl.text = timestampe
     }
 
 }
